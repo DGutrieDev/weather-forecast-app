a1c3202ec7619281394702e8b4806399adad736e
DGutrieDev <dgutrie.dev@gmail.com>
2024-03-10 09:52:54 -0600

    Bugs Fixed

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <FontAwesomeIcon icon="home" style={{color:'white'}}/>
+    <FontAwesomeIcon icon="" style={{color:'white'}}/>
   </React.StrictMode>
 );
 
